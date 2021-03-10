b1c92bfcd7001380fe67fc102408c080221eb054
Wil Neeley <wil.neeley@venafi.com>
2021-03-10 11:35:11 -0900

    Completed add/insertion database management functions

--- src/backend/sm_db_manager.js
+++ src/backend/sm_db_manager.js
@@ -51,14 +51,15 @@ class DBManager {
      */
     run_sql_file = (file_name) => {
         const dataSQL = fs.readFileSync(__dirname + '/sql/' + file_name).toString()
-        const dataArr = dataSQL.split(');')
+        const dataArr = dataSQL.split(';')
         this.DB.serialize(() => {
             this.DB.run('BEGIN TRANSACTION;')
             dataArr.forEach((query) => {
                 if (query) {
-                    query += ');'
+                    query += ';'
                     this.DB.run(query, (err) => {
                         if (err) throw err;
+                        else console.log("SMDB: Building table.")
                     })
                 }
             })
@@ -72,9 +73,133 @@ class DBManager {
     build_tables = () => {
         this.run_sql_file('sm_schema.sql')
     }
+
+    /**
+     * WARNING!!! - Completely removes all tables from the database. This should never
+     * be used outside of development.
+     */
+    drop_tables = () => {
+        this.run_sql_file('sm_drop_tables.sql')
+    }
+
+    /**
+     * Add an entry to the Profiles table.
+     */
+    add_profile_entry = (profile_name) => {
+        let sql = "INSERT INTO Profiles (profile_name) "
+        sql += "VALUES (?) "
+        this.DB.run(sql, [profile_name], function (err) {
+            if (err) {
+                console.log("SMDB:" + err)
+            } else {
+                console.log("SMDB: Profiles: Last ID: " + this.lastID)
+                console.log("SMDB: Profiles: # of Row Changes: " + this.changes)
+            }
+        })
+    }
+
+    /**
+     * Add an entry to the Stock_Orders or Stock_Simulations table.
+     */
+    add_stock_orders_entry = (options) => {
+        let
+            simulation = options.simulation || false,
+            table = simulation ? 'Stock_Simulations' : 'Stock_Orders',
+            uuid = options.uuid,
+            profile = options.profile,
+            market = options.market,
+            order_type = options.order_type,
+            symbol = options.symbol,
+            price = options.price,
+            shares = options.shares,
+            cost_basis = options.cost_basis,
+            limit_buy = options.limit_buy,
+            limit_sell = options.limit_sell,
+            order_date = options.order_date,
+            exec_date = options.exec_date
+        let sql = `INSERT INTO ${table} (uuid, profile, market, order_type, symbol, price, shares, cost_basis, limit_buy, limit_sell, order_date, exec_date) `
+        sql += "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) "
+        this.DB.run(sql, [uuid, profile, market, order_type, symbol, price, shares, cost_basis, limit_buy, limit_sell, order_date, exec_date], function (err) {
+            if (err) {
+                console.log("SMDB:" + err)
+            } else {
+                console.log(`SMDB: ${table}: Last ID: ` + this.lastID)
+                console.log(`SMDB: ${table}: # of Row Changes: ` + this.changes)
+            }
+        })
+    }
+
+    /**
+     * Add an entry to the Stock_Holdings table.
+     */
+    add_stock_holdings_entry = (uuid, profile, market, symbol, shares, price, cost_basis, simulated) => {
+        let sql = "INSERT INTO Stock_Holdings (uuid, profile, market, symbol, shares, price, cost_basis, simulated) "
+        sql += "VALUES (?, ?, ?, ?, ?, ?, ?, ?) "
+        this.DB.run(sql, [uuid, profile, market, symbol, shares, price, cost_basis, simulated], function (err) {
+            if (err) {
+                console.log("SMDB:" + err)
+            } else {
+                console.log("SMDB: Stock_Holdings: Last ID: " + this.lastID)
+                console.log("SMDB: Stock_Holdings: # of Row Changes: " + this.changes)
+            }
+        })
+    }
+
+    /**
+     * Add an entry to the Stock_Records table.
+     */
+    add_stock_record_entry = (market, symbol, quote, timestamp) => {
+        let sql = "INSERT INTO Stock_Records (market, symbol, quote, timestamp) "
+        sql += "VALUES (?, ?, ?, ?) "
+        this.DB.run(sql, [market, symbol, quote, timestamp], function (err) {
+            if (err) {
+                console.log("SMDB:" + err)
+            } else {
+                console.log("SMDB: Stock_Records: Last ID: " + this.lastID)
+                console.log("SMDB: Stock_Records: # of Row Changes: " + this.changes)
+            }
+        })
+    }
 }
 
+module.exports = {
+    DBManager: DBManager
+}
 
 //@todo - Testing and development
-let DPM = new DBManager()
-DPM.build_tables()
+let DBM = new DBManager()
+
+// DBM.build_tables()
+
+// DBM.drop_tables()
+
+// DBM.add_profile_entry("test_profile")
+
+// DBM.add_stock_record_entry("CRYPTO", "BTC", "100000", Date.now())
+
+// DBM.add_stock_orders_entry({
+//     simulation: true,
+//     uuid: '2389-23982-fss-232',
+//     profile: 'test_profile',
+//     market: 'CRYPTO',
+//     order_type: 'buy',
+//     symbol: 'BTC',
+//     price: 32092,
+//     shares: 0.02392,
+//     cost_basis: 5000,
+//     limit_buy: 0,
+//     limit_sell: 0,
+//     order_date: Date.now(),
+//     exec_date: Date.now()
+// })
+
+// DBM.add_stock_holdings_entry(
+//     "sdf-239-sdf",
+//     "test_profile",
+//     "CRYPTO",
+//     "BTC",
+//     "0.2345",
+//     "49342",
+//     "5000",
+//     true
+// )
--- src/backend/sql/sm_schema.sql
+++ src/backend/sql/sm_schema.sql
@@ -1,15 +1,16 @@
 CREATE TABLE IF NOT EXISTS `Profiles` (
-    `id`      	    INTEGER NOT NULL PRIMARY KEY,
-    `profile_name`	TEXT NOT NULL UNIQUE
+    `id`            INTEGER NOT NULL PRIMARY KEY,
+    `profile_name`  TEXT NOT NULL UNIQUE
 );
 CREATE TABLE IF NOT EXISTS `Stock_Orders` (
-    `id`      	    INTEGER NOT NULL PRIMARY KEY,
-    `uuid`  	    TEXT NOT NULL UNIQUE,
-    `profile`	    TEXT NOT NULL,
-    `market`      	TEXT NOT NULL,
-    `order_type`   	TEXT NOT NULL,
-    `symbol`	    TEXT NOT NULL,
-    `shares`	    NUMERIC NOT NULL,
+    `id`            INTEGER NOT NULL PRIMARY KEY,
+    `uuid`          TEXT NOT NULL,
+    `profile`       TEXT NOT NULL,
+    `market`        TEXT NOT NULL,
+    `order_type`    TEXT NOT NULL,
+    `symbol`        TEXT NOT NULL,
+    `shares`        NUMERIC NOT NULL,
+    `price`         NUMERIC NOT NULL,
     `cost_basis`    NUMERIC NOT NULL,
     `limit_buy`     NUMERIC,
     `limit_sell`    NUMERIC,
@@ -18,13 +19,14 @@ CREATE TABLE IF NOT EXISTS `Stock_Orders` (
     FOREIGN KEY (profile) REFERENCES Profiles(profile_name)
 );
 CREATE TABLE IF NOT EXISTS `Stock_Simulations` (
-    `id`      	    INTEGER NOT NULL PRIMARY KEY,
-    `uuid`  	    TEXT NOT NULL UNIQUE,
-    `profile`	    TEXT NOT NULL,
-    `market`      	TEXT NOT NULL,
-    `order_type`   	TEXT NOT NULL,
-    `symbol`	    TEXT NOT NULL,
-    `shares`	    NUMERIC NOT NULL,
+    `id`            INTEGER NOT NULL PRIMARY KEY,
+    `uuid`          TEXT NOT NULL,
+    `profile`       TEXT NOT NULL,
+    `market`        TEXT NOT NULL,
+    `order_type`    TEXT NOT NULL,
+    `symbol`        TEXT NOT NULL,
+    `shares`        NUMERIC NOT NULL,
+    `price`         NUMERIC NOT NULL,
     `cost_basis`    NUMERIC NOT NULL,
     `limit_buy`     NUMERIC,
     `limit_sell`    NUMERIC,
@@ -34,21 +36,22 @@ CREATE TABLE IF NOT EXISTS `Stock_Simulations` (
 );
 CREATE TABLE IF NOT EXISTS `Stock_Holdings` (
     `id`      	    INTEGER NOT NULL PRIMARY KEY,
-    `uuid`  	    TEXT NOT NULL UNIQUE,
+    `uuid`  	    TEXT NOT NULL,
     `profile`	    TEXT NOT NULL,
     `market`      	TEXT NOT NULL,
     `symbol`	    TEXT NOT NULL UNIQUE,
     `shares`	    NUMERIC NOT NULL,
+    `price`         NUMERIC NOT NULL,
     `cost_basis`    NUMERIC NOT NULL,
     `simulated`     INTEGER NOT NULL,
     FOREIGN KEY (profile) REFERENCES Profiles(profile_name)
 );
 CREATE TABLE IF NOT EXISTS `Stock_Records` (
-    `id`      	    INTEGER NOT NULL PRIMARY KEY,
-    `market`      	TEXT NOT NULL,
-    `symbol`	    TEXT NOT NULL,
-    `quote`	        INTEGER NOT NULL,
-    `date`	        DATE NOT NULL
+    `id`            INTEGER NOT NULL PRIMARY KEY,
+    `market`        TEXT NOT NULL,
+    `symbol`        TEXT NOT NULL,
+    `quote`         INTEGER NOT NULL,
+    `timestamp`     DATE NOT NULL
 );
 CREATE TABLE IF NOT EXISTS `Config` (
     `open_pref`	        TEXT DEFAULT 'last',
